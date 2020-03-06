7615113c6d3030a2ed4c05a3af974e3c8bbdbbbc
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2020-03-06 17:46:28 +0200

    first commit

---
+++ src/main/java/ruslan/simakov/luckymessenger/LuckymessengerApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.luckymessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckymessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckymessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruslan/simakov/luckymessenger/LuckymessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.luckymessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckymessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
